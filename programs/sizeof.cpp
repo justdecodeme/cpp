b4826b61f0432c3a2e8410bc761b3a984330fec4
Rakesh <justdecodeme@hotmail.com>
2019-09-02 00:32:55 +0530

    from vs code

---
+++ programs/sizeof.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 4;
+    float f = 4.5;
+    double d = 10.3;
+    char c = 'a';
+    bool b = true;
+
+    cout << "int: " << sizeof(i) << endl;
+    cout << "float: " << sizeof(f) << endl;
+    cout << "double: " << sizeof(d) << endl;
+    cout << "char: " << sizeof(c) << endl;
+    cout << "bool: " << sizeof(b) << endl;
+}
