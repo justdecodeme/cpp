0fc521a2cbceba556613039c0201da571b5c0165
Rakesh <justdecodeme@hotmail.com>
2019-09-02 09:40:29 +0530

    new example added

---
+++ programs/taking-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    
+    cout << "Type two numbers to add: \n";
+    cin >> a >> b;
+    cout << "Sum of a and b: " << a + b << endl;
+
+    cout << "Sum of c and d: " << c + d << endl; // not necessarily 0
+}
