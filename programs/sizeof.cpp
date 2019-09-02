e6dedeeabc2348abc500e74b8602453f9be4bbcd
Rakesh <justdecodeme@hotmail.com>
2019-09-02 09:22:03 +0530

    info added

--- programs/sizeof.cpp
+++ programs/sizeof.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 int main() {
+    // Compiler assigns a garbage value to an uninitialised local variable in C++ Programming
+
     int i = 4;
     float f = 4.5;
     double d = 10.3;
     char c = 'a';
     bool b = true;
 
-    cout << "sizeof: \n";
+    cout << "sizeof (in bytes): \n";
 
     cout << "int: " << sizeof(i) << endl;
     cout << "float: " << sizeof(f) << endl;
