e1ae0dfee19926d49b3a4593c9bcd1b96a7c7bfe
Rakesh <justdecodeme@hotmail.com>
2019-09-28 14:36:31 +0530

    arrays folder added

--- programs/level-01/square-root.cpp
+++ programs/level-01/square-root.cpp
@@ -1,3 +1,7 @@
+// Given a number N, find its square root. You need to find and print only the integral part of 
+// square root of N.
+// For eg. if number given is 18, answer is 4.
+
 #include<iostream>
 using namespace std;
 
