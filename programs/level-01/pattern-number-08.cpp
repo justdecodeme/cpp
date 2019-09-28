d6b7c76a7025c8109269ef0d686942b39faafc7f
Rakesh <justdecodeme@hotmail.com>
2019-09-28 22:37:34 +0530

    new examples added

---
+++ programs/level-01/pattern-number-08.cpp
@@ -0,0 +1,38 @@
+//    1
+//   212
+//  32123
+// 4321234
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n, r = 1, c;
+
+    cin >> n;
+
+    while(r <= n) {
+
+        c = 1;
+        while(c <= n-r) {
+            cout << " ";
+            c++;
+        }
+
+        c = 1;
+        while(c <= r) {
+            cout << r-c+1;
+            c += 1;
+        }
+
+        c = 1;
+        while(c <= r-1) {
+            cout << c+1;
+            c += 1;
+        }
+        
+        cout << endl;
+        r += 1;
+    }
+}
